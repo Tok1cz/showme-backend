eee35ced8547513f8b8f7fe3df3be3680ac4453b
Konstantin Todte <konstantin.todte@tum.de>
2025-07-30 11:35:32 +0200

    chore: fix init_db 3

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -15,7 +15,10 @@ sudo -u postgres psql -tc "SELECT 1 FROM pg_database WHERE datname = '$DB_NAME'"
 # Grant privileges
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"
 
-# Install postgis if not exists
+# Install postgis extension if not exists
 sudo -u postgres psql -d $DB_NAME -tc "SELECT 1 FROM pg_extension WHERE extname = 'postgis'" | grep -q 1 || \
-  sudo -u postgres psql -d $DB_NAME -c "CREATE EXTENSION postgis;" || \
-    sudo -u postgres psql -d $DB_NAME -c "CREATE EXTENSION hstore;"
+  sudo -u postgres psql -d $DB_NAME -c "CREATE EXTENSION postgis;"
+
+# Install hstore extension if not exists
+sudo -u postgres psql -d $DB_NAME -tc "SELECT 1 FROM pg_extension WHERE extname = 'hstore'" | grep -q 1 || \
+  sudo -u postgres psql -d $DB_NAME -c "CREATE EXTENSION hstore;"
